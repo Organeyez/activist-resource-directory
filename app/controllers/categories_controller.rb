f94b70e52a8d49f69dd4c8c5b43803a267102d73
ikn02010 <ikn02010@gmail.com>
2017-11-24 18:53:19 -0500

    List categories on category index page and add link to category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < ApplicationController
 	def index
+		@categories = Category.order(:title)
 	end 
 
 	def show
+		@category = Category.find(params[:id])
 	end
 
 #for admin use only 
