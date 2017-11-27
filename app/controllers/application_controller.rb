151bc5b9b952c6069311d7920465e06b5352ab59
ikn02010 <ikn02010@gmail.com>
2017-11-27 14:00:13 -0500

    Complete all resource functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
-
+ 	def after_sign_in_path_for(resource)
+  		user_path(@user)
+	end
 end
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -19,11 +19,11 @@ class Users::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
-  # protected
+  protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    root_path
+  end
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
