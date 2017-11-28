7189b48836889ab5f5357b437849ab6251a7b35e
ikn02010 <ikn02010@gmail.com>
2017-11-28 14:17:07 -0500

    Complete collection functionality

---
+++ app/controllers/collection_resources_controller.rb
@@ -0,0 +1,26 @@
+class CollectionResourcesController < ApplicationController
+
+  def create
+    @collection = current_user.collections.find(params[:collection_resource][:collection_id])
+    @collection_resource = @collection.collection_resources.create(collection_resource_params)
+    if @collection_resource.save
+      redirect_to request.referer
+    else
+      @errors = @collection_resource.errors.full_messages 
+      redirect_to request.referer, notice: 'Resource was not added to this collection'
+    end
+  end
+
+  def destroy
+    @collection_resource = CollectionResource.find_by(resource_id: params[:resource_id], collection_id: params[:collection_id])
+    @collection_resource.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def collection_resource_params
+    params.require(:collection_resource).permit(:collection_id, :resource_id)
+  end
+
+end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,10 +5,12 @@ class Resource < ApplicationRecord
 	has_many 	:resource_tags
 	has_many	:tags, through: :resource_tags
 	has_many	:resources, through: :resource_tags
+	has_many    :collection_resources 
 	has_many	:favorites
 	has_many	:fans, through: :favorites 
 	has_many	:reviews 
 	has_many	:reviewers, through: :reviews, source: :author
 
 	validates :title, :description, :url, :author_id, :category_id, presence: true 
+	validates :title, length: { maximum: 100 }
 end
