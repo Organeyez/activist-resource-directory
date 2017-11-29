133bbdf597ea797f85609898d439859d2cdf1345
ikn02010 <ikn02010@gmail.com>
2017-11-29 13:41:21 -0500

    Complete homepage css and js functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,6 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@resources = @user.posted_resources
-		respond_to do |f|
-	       f.html { render :show}
-	       f.json { render json: @resources.to_json}
-	       f.js
-	    end
 	end
 
 
